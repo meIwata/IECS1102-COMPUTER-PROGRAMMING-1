b53409ff42e7b1472b6248511fa1b40625022041
waleilulu <waleilulu@gmail.com>
2025-03-03 23:54:43 +0800

    update

---
+++ src/java_0227/Parens.java
@@ -0,0 +1,14 @@
+package java_0227;
+public class Parens {
+    public static void main(String[] argv) {
+      int i = 1 + 3 * 5 + 7; // 先算 3*5
+      System.out.println("1 + 3 * 5 + 7   = " + i);
+          
+      i = (1 + 3) * 5 + 7; // 先算 1+3
+      System.out.println("(1 + 3) * 5 + 7 = " + i);
+          
+      i = 1 + 3 * (5 + 7); // 先算 5+7
+      System.out.println("1 + 3 * (5 + 7) = " + i);
+    }
+  }
+  
