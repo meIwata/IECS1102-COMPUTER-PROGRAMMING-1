fefe847d5c6c76eb9006cc1e0b3124b501896f0e
waleilulu <waleilulu@gmail.com>
2025-03-04 09:41:48 +0800

    0304

--- src/java_0304/CheckOil.java
+++ src/java_0304/CheckOil.java
@@ -10,10 +10,10 @@ public class CheckOil {
         // int liter = scanner.nextInt(); // 課本用法
         float liter = scanner.nextFloat(); // 油量有可能有小數，使用float會比較好
 
-        if (liter < 2) {
+        if (liter < 2) 
             System.out.println("油量已經不足，該加油了!"); // 有檢查到 會提示這一行
-            System.out.println("祝您行車平安"); // 無論如何偷會顯示這一行
-        }
+            System.out.println("祝您行車平安"); // 無論如何都會顯示這一行
+        
         scanner.close();
     }
 }
