e3e9820fb2d37a280f7cf7e8e7262f470244f713
DESKTOP-57S7PKJ\Lulu <waleilulu@gmail.com>
2025-03-17 20:54:06 +0800

    0317

--- src/java_0313/IsPrime.java
+++ src/java_0313/IsPrime.java
@@ -9,7 +9,7 @@ public class IsPrime {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         while (true) {
-            System.out.print("請輸入要檢查的數 (輸入 0 結束)：");
+            System.out.print("請輸入要檢查的數 (輸入 0 結束)： ");
             String str = br.readLine();
             int num = Integer.parseInt(str);
             if (num == 0)
@@ -19,33 +19,31 @@ public class IsPrime {
             // System.out.println(range);
 
             // for (int i = 2; i <= range; i++) {
-            //     if (num % i == 0) {
-            //         isPrime = false;
-            //         break;
-            //     }
+            // if (num % i == 0) {
+            // isPrime = false;
+            // break;
+            // }
             // }
             // if (isPrime) {
-            //     System.out.println(num + " 是質數");
+            // System.out.println(num + " 是質數");
             // } else {
-            //     System.out.println(num + " 不是質數");
+            // System.out.println(num + " 不是質數");
             // }
-
-            for (int i = 2; i <= range; i++) {
-                if (num % i == 0) {
-                    if (isPrime == true) {
-                        isPrime = false;
+            for (int i = 2; i <= range; i++) { // 除數範圍: 2 ~ range
+                if (num % i == 0) { // num 可被 i 整除
+                    if (isPrime) { 
+                        isPrime = false; // 不是質數的話
                         System.out.print(num + " 不是質數, 可被 " + i);
-                    }else {
+                    } else {
                         System.out.println(" " + i);
                     }
                 }
-            }if (isPrime) {
-            System.out.println(num +" 是質數");
             }
-            else{
-            System.out.println(" 整除");
+            if (isPrime) {
+                System.out.println(num + " 是質數");
+            } else {
+                System.out.println(" 整除");
             }
         }
-
     }
 }
