667ec8109c99ca632eee38f794d8ca5673d43adc
waleilulu <waleilulu@gmail.com>
2025-03-18 09:43:40 +0800

    0318

---
+++ src/java_0318/Ex_6d.java
@@ -0,0 +1,12 @@
+package java_0318;
+
+public class Ex_6d {
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int i = 0; i < 10; i++) {
+            sum = sum + i;
+            System.out.println(sum);
+            continue;
+        }
+    }
+}
