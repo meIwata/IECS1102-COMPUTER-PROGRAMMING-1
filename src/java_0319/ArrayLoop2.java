d66a8ff23a70d5fccb2a01467051377028d137f4
waleilulu <waleilulu@gmail.com>
2025-03-19 11:39:15 +0800

    0319

--- src/java_0319/ArrayLoop2.java
+++ src/java_0319/ArrayLoop2.java
@@ -1,18 +1,16 @@
 package java_0319;
 
-public class ArrayLoop {
+public class ArrayLoop2 {
     public static void main(String[] args) {
         double[] students = { 70, 65, 90, 85, 95 };
         double sum = 0;
-
-        // 增強型for迴圈
-        for(double score: students) { //每一個元素都取出來
+        for (int i = 0; i < students.length; i++) {
+            double score = students[i];
             sum += score; // 累積加總
         }
-        
         System.out.println("總分: " + sum);
-
-        double avg = sum/students.length; // 平均值
+        
+        double avg = sum / students.length; // 平均值
         System.out.println("平均: " + avg);
     }
 }
