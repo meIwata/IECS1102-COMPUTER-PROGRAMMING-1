2a416f09585be8097589925202cb90e2cb2ce62e
waleilulu <waleilulu@gmail.com>
2025-03-20 11:26:17 +0800

    0320

--- src/java_0319/ArrayLoop.java
+++ src/java_0319/ArrayLoop.java
@@ -5,10 +5,11 @@ public class ArrayLoop {
         double[] students = { 70, 65, 90, 85, 95 };
         double sum = 0;
 
-        // 增強型for迴圈
+        // 增強型for迴圈，將students陣列中的每一個元素取出來作為score
         for(double score: students) { //每一個元素都取出來
             sum += score; // 累積加總
         }
+        
         
         System.out.println("總分: " + sum);
 
--- src/java_0319/ArrayLoop2.java
+++ src/java_0319/ArrayLoop2.java
@@ -4,13 +4,21 @@ public class ArrayLoop2 {
     public static void main(String[] args) {
         double[] students = { 70, 65, 90, 85, 95 };
         double sum = 0;
+
+        // 傳統for迴圈，利用陣列的長度來控制迴圈
         for (int i = 0; i < students.length; i++) {
             double score = students[i];
             sum += score; // 累積加總
         }
+
         System.out.println("總分: " + sum);
         
         double avg = sum / students.length; // 平均值
         System.out.println("平均: " + avg);
+
+        System.out.println("--------------------");
+        //要取出陣列最後一個數值
+        //students[最後一個數值]，索引是length-1
+        System.out.println("陣列最後一個數值: " + students[students.length-1]);
     }
 }
