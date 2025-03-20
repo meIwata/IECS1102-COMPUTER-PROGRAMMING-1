99f325ed4ecfb248ee0f3f00784009dcea6ac37d
waleilulu <waleilulu@gmail.com>
2025-03-20 11:10:15 +0800

    0320

--- src/java_0319/ArrayInitWithExpr.java
+++ src/java_0319/ArrayInitWithExpr.java
@@ -3,7 +3,7 @@ package java_0319;
 public class ArrayInitWithExpr {
     public static void main(String[] args) {
         int i = 4, j = 8;
-        int[] a = { 4, i, i + j };
+        int[] a = { 4, i, i + j }; // int[] a = new int[] { 4, 4, 12 };
 
         for (int k = 0; k < a.length; k++) {
             System.out.println("a[" + k + "] = " + a[k]);
--- src/java_0319/ArrayWithExpr.java
+++ src/java_0319/ArrayWithExpr.java
@@ -4,6 +4,9 @@ public class ArrayWithExpr {
     public static void main(String[] args) {
         int i = 4, j = 8;
         int[] a = new int[i];
-        int[] b = new int[j - i];
+        int[] b = new int[j - i]; //int[] b = new int[4];
+
+        System.out.println("a的長度: " + a.length);
+        System.out.println("b的長度: " + b.length);
     }
 }
