9705d46dd665f3cfb3a1b7f74cdfd799b5c073ef
waleilulu <waleilulu@gmail.com>
2025-03-27 10:56:44 +0800

    0327

--- src/java_0327/ShowArgs.java
+++ src/java_0327/ShowArgs.java
@@ -2,8 +2,8 @@ package java_0327;
 
 public class ShowArgs {
     public static void main(String[] args) {
-        for(int i = 0;i<args.length;i++){
-            System.out.println("第" +i+"個參數: " + args[i]);
+        for (int i = 0; i < args.length; i++) {
+            System.out.println("第" + i + "個參數: " + args[i]);
         }
     }
 }
